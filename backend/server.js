7cfc57952a705eaa8a5987ca39e065fd8a8e0c89
pauliuszabinskas <zabinskas.paulius951@gmail.com>
2025-03-04 11:06:44 +0000

    update MongoDB connection URI and implement database connection logic

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,8 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectFB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +10,8 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {    
-  console.log('Server is running on http://localhost:3000');
+
+app.listen(3000, () => {
+    connectFB();
+    console.log('Server is running on http://localhost:3000');
 });
