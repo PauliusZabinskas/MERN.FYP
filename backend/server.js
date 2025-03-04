da18c8961f528a81d5901a41003be68ee4115c9f
pauliuszabinskas <zabinskas.paulius951@gmail.com>
2025-03-04 20:38:02 +0000

    update server configuration to use environment variable for port and add PORT to .env file

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ app.get('/', (req, res) => {
 app.use('/api/file', fileRoutes);
 
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:'+PORT);
 });
