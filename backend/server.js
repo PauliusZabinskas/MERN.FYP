d2ca05fb6629673dee33b43a3850fa5e0b7e37c5
pauliuszabinskas <zabinskas.paulius951@gmail.com>
2025-04-01 23:18:16 +0100

    update server port to 5500; modify environment variables and adjust frontend proxy settings

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5500;
 
 // Connect to MongoDB before starting the server
 connectDB()
