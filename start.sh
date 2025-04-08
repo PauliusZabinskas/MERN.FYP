7d353b607b863fcbcf6c10b4a220746045a720fb
pauliuszabinskas <zabinskas.paulius951@gmail.com>
2025-04-08 16:08:55 +0100

    refactor docker-compose and scripts; remove zap service, update disk usage, and adjust file permissions

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-docker-compose up -d
+docker-compose down -v  # Clean up previous containers if any
+docker-compose up -d # Start the services in detached mode
 npm run dev
 cd frontend && npm run dev
